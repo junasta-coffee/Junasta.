e870e5389baa6a75c4ea4d91564e4954d68720c8
junasta-coffee <junasta51@gmail.com>
2025-04-19 05:25:21 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const navLinks = document.querySelectorAll(".nav-menu .nav-link");
 const menuOpenButton = document.querySelector("#menu-open-button");
 const menuCloseButton = document.querySelector("#menu-close-button");
 
@@ -7,4 +8,41 @@ menuOpenButton.addEventListener("click", () => {
 
 menuCloseButton.addEventListener("click", () => {
     document.body.classList.remove("show-mobile-menu");
+});
+
+navLinks.forEach( link => {
+ link.addEventListener("click", () => {document.body.classList.remove("show-mobile-menu")});
+});
+
+// initialize Swiper
+const swiper = new Swiper(".slider-wrapper", {
+    loop: true,
+    grabCursor:true,
+    spaceBetween: 25,
+
+    // If we need pagination
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets: true
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+    },
+
+    // Responsive break point
+    breakpoints: {
+        0: {
+            slidesPerView: 1
+        },
+        768: {
+            slidesPerView: 2
+        },
+        1024: {
+            slidesPerView: 3
+        }
+    }
 });
